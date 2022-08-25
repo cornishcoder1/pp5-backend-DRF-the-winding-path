ee11ab1e047174b8ef0d1d0e56749b5411ef0689
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-08-25 14:53:45 +0000

    Added Gallery Post Detail view to retrieve, edit and delete gallery posts

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,5 @@ from gallery import views
 
 urlpatterns = [
     path('gallery-posts/', views.GalleryPostsList.as_view()),
+    path('gallery-posts/<int:pk>/', views.GalleryPostDetail.as_view()),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from rest_framework import status, permissions
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Gallery
 from .serializers import GallerySerializer
 
@@ -32,4 +34,49 @@ class GalleryPostsList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class GalleryPostDetail(APIView):
+    """Gallery Post Detail view"""
+    serializer_class = GallerySerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        """Method to handle requests made for a
+        gallery post which does not exist"""
+        try:
+            gallery_post = Gallery.objects.get(pk=pk)
+            self.check_object_permissions(self.request, gallery_post)
+            return gallery_post
+        except Gallery.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """Get method to return a single Gallery Post instance"""
+        gallery_post = self.get_object(pk)
+        serializer = GallerySerializer(
+            gallery_post,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        """Put method to update a Gallery Post instance"""
+        gallery_post = self.get_object(pk)
+        serializer = GallerySerializer(
+            gallery_post, data=request.data,
+            context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """Delete method to delete a Gallery Post instance"""
+        gallery_post = self.get_object(pk)
+        gallery_post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
