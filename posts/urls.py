89eb78ed627a5959455cae6b7fe1752741aa9615
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-08-27 09:19:01 +0000

    Created Walk Post Detail view to retrieve edit and delete posts

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from posts import views
 
 urlpatterns = [
     path('walk-posts/', views.WalkPostsList.as_view()),
+    path('walk-posts/<int:pk>/', views.WalkPostDetail.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -37,46 +37,47 @@ class WalkPostsList(APIView):
         )
 
 
-# class GalleryPostDetail(APIView):
-#     """Gallery Post Detail view"""
-#     serializer_class = GallerySerializer
-#     permission_classes = [IsOwnerOrReadOnly]
+class WalkPostDetail(APIView):
+    """Walk Post Detail view"""
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
-#     def get_object(self, pk):
-#         """Method to handle requests made for a
-#         gallery post which does not exist"""
-#         try:
-#             gallery_post = Gallery.objects.get(pk=pk)
-#             self.check_object_permissions(self.request, gallery_post)
-#             return gallery_post
-#         except Gallery.DoesNotExist:
-#             raise Http404
+    def get_object(self, pk):
+        """Method to handle requests made for a
+        walk post which does not exist"""
+        try:
+            walk_post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, walk_post)
+            return walk_post
+        except Post.DoesNotExist:
+            raise Http404
 
-#     def get(self, request, pk):
-#         """Get method to return a single Gallery Post instance"""
-#         gallery_post = self.get_object(pk)
-#         serializer = GallerySerializer(
-#             gallery_post,
-#             context={'request': request}
-#         )
-#         return Response(serializer.data)
+    def get(self, request, pk):
+        """Get method to return a single Walk Post instance"""
+        walk_post = self.get_object(pk)
+        serializer = PostSerializer(
+            walk_post,
+            context={'request': request}
+        )
+        return Response(serializer.data)
 
-#     def put(self, request, pk):
-#         """Put method to update a Gallery Post instance"""
-#         gallery_post = self.get_object(pk)
-#         serializer = GallerySerializer(
-#             gallery_post, data=request.data,
-#             context={'request': request}
-#         )
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk):
+        """Put method to update a Walk Post instance"""
+        walk_post = self.get_object(pk)
+        serializer = PostSerializer(
+            walk_post, data=request.data,
+            context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def delete(self, request, pk):
-#         """Delete method to delete a Gallery Post instance"""
-#         gallery_post = self.get_object(pk)
-#         gallery_post.delete()
-#         return Response(
-#             status=status.HTTP_204_NO_CONTENT
-#         )
+    def delete(self, request, pk):
+        """Delete method to delete a Walk Post instance"""
+        walk_post = self.get_object(pk)
+        walk_post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+
