1acec0ac592604a134f8dbe52a58bb707bf0d463
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-08-31 13:08:45 +0000

    Alter comments urls for walk and gallery comments to a hyphen instead of underscore

--- comments_gallery/urls.py
+++ comments_gallery/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from comments_gallery import views
 
 urlpatterns = [
-    path('comments_gallery/', views.GalleryCommentList.as_view()),
-    path('comments_gallery/<int:pk>/', views.GalleryCommentDetail.as_view()),
+    path('comments-gallery/', views.GalleryCommentList.as_view()),
+    path('comments-gallery/<int:pk>/', views.GalleryCommentDetail.as_view()),
 ]
--- comments_walk/urls.py
+++ comments_walk/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from comments_walk import views
 
 urlpatterns = [
-    path('comments_walk/', views.WalkCommentList.as_view()),
-    path('comments_walk/<int:pk>/', views.WalkCommentDetail.as_view()),
+    path('comments-walk/', views.WalkCommentList.as_view()),
+    path('comments-walk/<int:pk>/', views.WalkCommentDetail.as_view()),
 ]
