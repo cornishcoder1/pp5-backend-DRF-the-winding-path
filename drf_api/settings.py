b294052b442ef321ced37d5553fbdf967c05317a
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-11-19 12:50:23 +0000

    Prepare repo for deployment to Render

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,6 +69,11 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # if 'CLIENT_ORIGIN_DEV' in os.environ:
 #     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''),
 #     re.IGNORECASE).group(0)
